831ecba63fb826e54b754c9b6184a6b1357f61af
a lan <bluenowte97@gmail.com>
2018-11-18 18:04:48 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -4,7 +4,11 @@ def load_library(path)
   thing = {}
   thing = YAML.load_file("./lib/emoticons.yml") # code goes here
   puts thing.inspect
-  thing
+  thing.each do |key, value|
+    thing[get_meaning] = key
+    thing[get_emoticon] = value
+  end
+  thing  
 end
 
 def get_japanese_emoticon
